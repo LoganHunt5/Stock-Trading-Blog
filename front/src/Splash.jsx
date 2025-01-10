5962e9c7d0f251cd4737420548b1740cb7920c58
Logan <l.b.hunt5@gmail.com>
2025-01-09 19:01:46 -0800

    splash changes

--- front/src/Splash.jsx
+++ front/src/Splash.jsx
@@ -1,45 +1,61 @@
+import { useState, useRef } from "react";
 import line from "./images/SplashTitleLine.png";
-import candlestick from "./images/Candlestick.png";
+import circles from "./images/SplashCircles.png";
 
 function Splash() {
-  return (
-    <div className="container">
-      <div className="container splashheader">
-        <img
-          src={line}
-          alt="line"
-          className="splashheaderline"
-          draggable="false"
-        ></img>
-        <h1 className="splashheaderplace">PLACE</h1>
-        <h1>HOLDER</h1>
-      </div>
-      <div className="container splashtextandbutton">
-        <h2>
-          <span id="onestop">Your one stop shop for analyzing</span>{" "}
-          <span id="pennystocks">penny stocks.</span>
-        </h2>
-        <ul>
-          <li>Filter stocks by any metric</li>
-          <li>Sentiment analysis</li>
-          <li>NEW - track other trader&apos;s portfolios</li>
-        </ul>
-        <button className="splashgetstarted">Get Started</button>
-      </div>
-      <div className="splashellipse1"></div>
+  /*<div className="splashellipse1"></div>
       <div className="splashellipse2"></div>
       <div className="splashellipse3"></div>
       <div className="splashellipse4"></div>
       <div className="splashellipse5"></div>
-      <div className="splashellipse6"></div>
-      <img
-        src={candlestick}
-        alt="candlestick"
-        className="spashcandlestick"
-        draggable="false"
-      ></img>
-    </div>
-  );
+      <div className="splashellipse6"></div>*/
+  const [showElement, setShowElement] = useState(true);
+  const splashContainer = useRef();
+  const handleButtonClick = () => {
+    splashContainer.current.style.opacity = 0;
+    setTimeout(function () {
+      setShowElement(false);
+    }, 1000);
+  };
+  if (showElement) {
+    return (
+      <div className="container splashcontainer" ref={splashContainer}>
+        <div className="container splashheader">
+          <img
+            src={line}
+            alt="line"
+            className="splashheaderline"
+            draggable="false"
+          ></img>
+          <h1 className="splashheaderplace">TESTING</h1>
+          <h1 className="splashheaderlog">LOG</h1>
+        </div>
+        <div className="container splashtextandbutton">
+          <h2>
+            <span id="onestop">Follow along with me on my day</span>{" "}
+            <span id="pennystocks">trading and web design journeys</span>
+          </h2>
+          <ul>
+            <li id="splashli1">Started this blog on: 1/9/25</li>
+            <li id="splashli2">Weekly updates for both trading and coding</li>
+            <li id="splashli3">Day trading S&P/NASDAQ futures day: 0</li>
+            <li id="splashli4">I&apos;m a current Computer Science Junior</li>
+          </ul>
+          <button className="splashgetstarted" onClick={handleButtonClick}>
+            Welcome!
+          </button>
+        </div>
+        <img
+          src={circles}
+          alt="circles"
+          className="splashcircles"
+          draggable="false"
+        ></img>
+      </div>
+    );
+  } else {
+    return;
+  }
 }
 
 export default Splash;
